f6ef12e05a59f632282ccaa9cf1912bbfd3d22e3
soph-yang <thesophiayang2001@gmail.com>
2021-04-28 02:55:34 -0400

    now the header of csv is not added to database

--- insertdb.js
+++ insertdb.js
@@ -3,6 +3,7 @@ const url = "mongodb+srv://sophiayang:123qweasd@cluster0.c2ncf.mongodb.net/stock
 
 var fs = require('fs');
 var readline = require('readline');
+const parse = require('csv-parse');
 
 function main() 
 {
@@ -16,29 +17,26 @@ function main()
 
         //creating array to insert using readline module
         const company_array = [];
-        var myFile = readline.createInterface({
-            input: fs.createReadStream('companies.csv')
-        });
-        myFile.on('line', function(line) {
-            lineArr = line.split(',');    
-            company_array.push({
-                company: lineArr[0],
-                ticker: lineArr[1] 
+        fs.createReadStream('companies.csv')
+            .pipe(parse({ delimiter: ',', from_line: 2 })) //starts from second row of csv
+            .on('data', (row) => {
+                company_array.push({
+                    company: row[0],
+                    ticker: row[1] 
+                })
             })
-        });
-        
-        //inserting array into MongoDB database
-        myFile.on('close', function() {
-           console.log('Inserting this array: ', company_array);
-           collection.insertMany(company_array, function(err, res) {
-               if(err) { 
-                   console.log("query err: " + err); return; 
-               }
-               console.log("new documents inserted");
-           });
+            .on('close', function() {
+               //inserting array into MongoDB database
+               console.log('Inserting this array: ', company_array);
+               collection.insertMany(company_array, function(err, res) {
+                   if(err) { 
+                       console.log("query err: " + err); return; 
+                   }
+                   console.log("new documents inserted");
+               });
 
-           console.log("Success!");
-        });
+               console.log("Success!");
+            });
     });
 }
 
